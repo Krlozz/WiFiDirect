0255a1b1597987ce5bec2de4fe4c287af4935630
Krlozz Ayala <k_arlos210@hotmail.com>
2017-02-15 22:21:28 -0500

    Update WiFiChatFragment.java

--- Programa/ChatFIS-master/app/src/main/java/it/polimi/deib/p2pchat/discovery/chatmessages/WiFiChatFragment.java
+++ Programa/ChatFIS-master/app/src/main/java/it/polimi/deib/p2pchat/discovery/chatmessages/WiFiChatFragment.java
@@ -1,20 +1,6 @@
 
 package it.polimi.deib.p2pchat.discovery.chatmessages;
-/*
- * Copyright (C) 2015-2016 Stefano Cappa
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -37,12 +23,6 @@ import it.polimi.deib.p2pchat.discovery.services.WiFiP2pService;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Class fragment that handles chat related UI which includes a list view for messages
- * and a message entry field with send button.
- * <p></p>
- * Created by Stefano Cappa on 04/02/15, based on google code samples.
- */
 public class WiFiChatFragment extends Fragment {
 
     private static final String TAG = "WiFiChatFragment";
@@ -57,34 +37,16 @@ public class WiFiChatFragment extends Fragment {
     @Getter @Setter private ChatManager chatManager;
     private WiFiChatMessageListAdapter adapter = null;
 
-    /**
-     * Callback interface to call methods reconnectToService in {@link it.polimi.deib.p2pchat.discovery.MainActivity}.
-     * MainActivity implements this interface.
-     */
     public interface AutomaticReconnectionListener {
         public void reconnectToService(WiFiP2pService wifiP2pService);
     }
 
-    /**
-     * Method to obtain a new Fragment's instance.
-     * @return This Fragment instance.
-     */
     public static WiFiChatFragment newInstance() {
         return new WiFiChatFragment();
     }
 
-    /**
-     * Default Fragment constructor.
-     */
     public WiFiChatFragment() {}
 
-
-    /**
-     * Method that combines all the messages inside the
-     * {@link it.polimi.deib.p2pchat.discovery.chatmessages.waitingtosend.WaitingToSendQueue}
-     * in one String and pass this one to the {@link it.polimi.deib.p2pchat.discovery.socketmanagers.ChatManager}
-     * to send the message to other devices.
-     */
     public void sendForcedWaitingToSendQueue() {
 
         Log.d(TAG, "sendForcedWaitingToSendQueue() called");
@@ -111,41 +73,25 @@ public class WiFiChatFragment extends Fragment {
         }
     }
 
-
-    /**
-     * Method to add a message to the Fragment's listView and notifies this update to
-     * {@link it.polimi.deib.p2pchat.discovery.chatmessages.WiFiChatMessageListAdapter}.
-     * @param readMessage String that represents the message to add.
-     */
     public void pushMessage(String readMessage) {
         items.add(readMessage);
         adapter.notifyDataSetChanged();
     }
 
-    /**
-     * Method that updates the {@link it.polimi.deib.p2pchat.discovery.chatmessages.WiFiChatMessageListAdapter}.
-     */
     public void updateChatMessageListAdapter() {
         if(adapter!=null) {
             adapter.notifyDataSetChanged();
         }
     }
 
-    /**
-     * Method that add the text in the chatLine EditText to the WaitingToSendQueue and try to reconnect
-     * to the service associated to the device of this tab, with index tabNumber.
-     */
     private void addToWaitingToSendQueueAndTryReconnect() {
-        //add message to the waiting to send queue
         WaitingToSendQueue.getInstance().getWaitingToSendItemsList(tabNumber).add(chatLine.getText().toString());
 
-        //try to reconnect
         WifiP2pDevice device = DestinationDeviceTabList.getInstance().getDevice(tabNumber - 1);
         if(device!=null) {
             WiFiP2pService service = ServiceList.getInstance().getServiceByDevice(device);
             Log.d(TAG, "device address: " + device.deviceAddress + ", service: " + service);
 
-            //call reconnectToService in MainActivity
             ((AutomaticReconnectionListener) getActivity()).reconnectToService(service);
 
         } else {
@@ -172,7 +118,6 @@ public class WiFiChatFragment extends Fragment {
                             if (!chatManager.isDisable()) {
                                 Log.d(TAG, "chatmanager state: enable");
 
-                                //send message to the ChatManager's outputStream.
                                 chatManager.write(chatLine.getText().toString().getBytes());
                             } else {
                                 Log.d(TAG, "chatmanager disabled, trying to send a message with tabNum= " + tabNumber);
@@ -190,6 +135,4 @@ public class WiFiChatFragment extends Fragment {
 
         return view;
     }
-
-
 }
